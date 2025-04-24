34bfba1f4ef185d918249480216cc25f75946427
BoutainaBoudiouane <boutainaboudiouane@gmail.com>
2025-04-24 14:19:28 +0000

    update_task_4

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-"""Contains definition of variables with specified values and annotations."""
+"""Define and annotate the following variables with the specified values:
+a, an integer with a value of 1
+pi, a float with a value of 3.14
+i_understand_annotations, a boolean with a value of True
+school, a string with a value of “Holberton”
+"""
+
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str =  "HOLBERTON"
+school: str = 'Holberton'
